1e2d3bb494f945f510b07ff1780c9bd81c30cb15
Ttam040402 <vothithanhtam.020404@gmail.com>
2025-05-28 09:21:45 +0700

    Thêm Form để thêm liên hệ mới 2280602853_VoThiThanhTam

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -36,9 +36,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(62, 42);
+            this.label1.Location = new System.Drawing.Point(55, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 20);
+            this.label1.Size = new System.Drawing.Size(122, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Danh Sách Liên Hệ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -46,29 +46,31 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(501, 42);
+            this.label2.Location = new System.Drawing.Point(445, 34);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.Size = new System.Drawing.Size(0, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Thêm Liên Hệ ";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(511, 129);
+            this.button1.Location = new System.Drawing.Point(454, 103);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 41);
+            this.button1.Size = new System.Drawing.Size(165, 33);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Lưu";
+            this.button1.Text = "Them lien he";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.Form1_Load);
